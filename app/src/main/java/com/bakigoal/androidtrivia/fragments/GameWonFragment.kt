327c61d6ac307589811f85d17f03fbd6db317fe4
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-09 17:58:01 +0500

    hide share if there is no app to handle intent

--- app/src/main/java/com/bakigoal/androidtrivia/fragments/GameWonFragment.kt
+++ app/src/main/java/com/bakigoal/androidtrivia/fragments/GameWonFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -41,13 +42,18 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.winner_menu, menu)
+
+        // hide share menu if no app can handle intent
+        if (getShareIntent().resolveActivity(requireActivity().packageManager) == null) {
+            menu.findItem(R.id.share).isVisible = false
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.share -> shareSuccess()
         }
-        return  super.onOptionsItemSelected(item)
+        return super.onOptionsItemSelected(item)
     }
 
     private fun getShareIntent(): Intent {
@@ -58,6 +64,14 @@ class GameWonFragment : Fragment() {
                 Intent.EXTRA_TEXT,
                 getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
             )
+    }
+
+    private fun getShareIntentCompat(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        return ShareCompat.IntentBuilder(requireActivity())
+            .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            .setType("text/plain")
+            .intent
     }
 
     private fun shareSuccess() {
