07af4242dead1e03e56d2e595b7ff75fc6901e48
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-12-09 13:28:25 +0500

    add navigation

--- app/src/main/java/com/bakigoal/androidtrivia/fragments/TitleFragment.kt
+++ app/src/main/java/com/bakigoal/androidtrivia/fragments/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.bakigoal.androidtrivia.R
 import com.bakigoal.androidtrivia.databinding.FragmentTitleBinding
 
@@ -18,6 +19,14 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
         )
+        setListeners(binding)
         return binding.root
+    }
+
+    private fun setListeners(binding: FragmentTitleBinding) {
+        binding.playButton.setOnClickListener {
+            val navController = Navigation.findNavController(it)
+            navController.navigate(R.id.action_titleFragment_to_gameFragment)
+        }
     }
 }
